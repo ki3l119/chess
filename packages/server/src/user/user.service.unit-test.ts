61d3a6628a0c2ec4b90e7cfaff4636d66d008315
Joshua Kiel Gaurano <joshuakielg@gmail.com>
2024-11-23 18:15:31 +0800

    [server] Add login service unit tests

--- packages/server/src/user/user.service.unit-test.ts
+++ packages/server/src/user/user.service.unit-test.ts
@@ -1,9 +1,10 @@
+import bcrypt from "bcrypt";
 import { describe, jest, it, expect } from "@jest/globals";
 
 import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service";
 import { afterEach } from "node:test";
-import { CreateUserDto, UserDto } from "chess-shared-types";
+import { CreateUserDto, LoginDto, UserDto } from "chess-shared-types";
 import {
   DuplicateEmailException,
   DuplicateUsernameException,
@@ -12,6 +13,8 @@ import {
 describe("UserService", () => {
   const userRepositoryMock = {
     insert: jest.fn<typeof UserRepository.prototype.insert>(),
+    findByEmail: jest.fn<typeof UserRepository.prototype.findByEmail>(),
+    insertSession: jest.fn<typeof UserRepository.prototype.insertSession>(),
   };
   const userService = new UserService(
     userRepositoryMock as unknown as UserRepository,
@@ -82,6 +85,81 @@ describe("UserService", () => {
       });
 
       await expect(actual).rejects.toBeInstanceOf(DuplicateUsernameException);
+    });
+  });
+
+  describe("login", () => {
+    it("Creates new session on valid credentials", async () => {
+      const input: LoginDto = {
+        email: "test@gmail.com",
+        password: "p@ssword",
+      };
+
+      const dummyUserId = "6680afcd-2e05-45b7-9376-b5a48473e101";
+      const dummySessionId = "cc94cd85-8b7e-42b1-b594-8de55bd641d5";
+
+      userRepositoryMock.findByEmail.mockImplementationOnce((email) => {
+        const creationDate = new Date();
+        creationDate.setDate(creationDate.getDate() - 60);
+        return Promise.resolve({
+          id: dummyUserId,
+          username: "test",
+          createdAt: creationDate,
+          password: bcrypt.hashSync(input.password, 10),
+          email,
+        });
+      });
+
+      userRepositoryMock.insertSession.mockImplementationOnce((newSession) => {
+        return Promise.resolve({
+          id: dummySessionId,
+          userId: dummyUserId,
+          createdAt: newSession.createdAt || new Date(),
+          expiresAt: newSession.expiresAt,
+        });
+      });
+
+      const actual = await userService.login(input);
+
+      expect(actual).not.toBeNull();
+      expect(actual!.id).toEqual(dummySessionId);
+    });
+
+    it("Returns null if no user has the given email", async () => {
+      const input: LoginDto = {
+        email: "test@gmail.com",
+        password: "p@ssword",
+      };
+
+      userRepositoryMock.findByEmail.mockResolvedValueOnce(null);
+
+      const actual = await userService.login(input);
+
+      expect(actual).toBeNull();
+    });
+
+    it("Returns null if wrong password", async () => {
+      const input: LoginDto = {
+        email: "test@gmail.com",
+        password: "p@ssword",
+      };
+
+      const dummyUserId = "6680afcd-2e05-45b7-9376-b5a48473e101";
+
+      userRepositoryMock.findByEmail.mockImplementationOnce((email) => {
+        const creationDate = new Date();
+        creationDate.setDate(creationDate.getDate() - 60);
+        return Promise.resolve({
+          id: dummyUserId,
+          username: "test",
+          createdAt: creationDate,
+          password: bcrypt.hashSync("origina-password", 10),
+          email,
+        });
+      });
+
+      const actual = await userService.login(input);
+      expect(actual).toBeNull();
     });
   });
 });
