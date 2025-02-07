afed80a088fdaeddb6d71efc0db7f8c4942e4d8d
Joshua Kiel Gaurano <joshuakielg@gmail.com>
2025-02-07 08:29:12 +0800

    [game] Export `Board` class

--- packages/game/src/index.ts
+++ packages/game/src/index.ts
@@ -1,5 +1,5 @@
 export { PieceColor } from "./pieces";
 export { startingBoardFENString, parseFEN } from "./utils/fen-parser";
-export { Move } from "./board";
+export { Move, Board } from "./board";
 export { ChessException, InvalidMoveException } from "./exceptions";
 export { Chess } from "./chess";
